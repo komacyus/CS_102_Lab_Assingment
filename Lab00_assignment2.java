a002026a3b5eebcc0f85bc86a63a29eedf14ab52
İdil Su Öztürk <suidil1234@gmail.com>
2022-09-21 15:48:17 +0300

    finding min and max

--- Lab00_assignment2.java
+++ Lab00_assignment2.java
@@ -3,4 +3,152 @@ public class Lab00_assignment2 {
     public static void main(String[] args) {
         
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * finds the max value in givven array
+     * @param array
+     * @return max integer
+     */
+    public int findMax (int array[]) {
+        int max = array[0];
+
+         for (int i = 1; i < array.length; i++)
+             if (array[i] > max) {
+                max = array[i];
+             }
+       
+         return max;
+    }
+
+    /**
+     * finds the min value in givven array
+     * @param array
+     * @return min integer
+     */
+    public int findMin (int array[]) {
+        int min = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+
+        return min;
+    } 
 }
