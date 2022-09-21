1ee8b517e304c5daef86b811217c8b64937bbbc4
Canberk <canberk1223@gmail.com>
2022-09-21 15:42:03 +0300

    Auto stash before merge of "main" and "origin/main"

--- Lab00_assignment2.java
+++ Lab00_assignment2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lab00_assignment2 {
 
     public static void main(String[] args) {
@@ -6,5 +8,86 @@ public class Lab00_assignment2 {
             numbers[i] = i;
         }
         System.out.println(numbers[101]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        Scanner in = new Scanner(System.in);
+        boolean isExit = true;
+        int choice;
+        String s1 = "Menu%n 1-min and max values of the array %n 2-average of the array% 3-odd sum%n 4-even sum %n 5-exit";
+
+        while (isExit) {
+            System.out.println(s1);
+            System.out.print("Enter a choice");
+            choice = in.nextInt();
+
+            if ( choice == 1) { // min max
+
+                
+            }
+            else if( choice == 2){ //average
+                
+
+            }
+            else if ( choice == 3) { //odd sum
+                
+            }
+            else if ( choice == 4) { //even sum
+                
+            }
+            else if ( choice == 5) { //exit
+                System.out.println("Exitting!");
+                isExit = false;
+                
+            }
+            else{
+                System.out.println( "please enter a number between 1-5 !");
+            }
+
+            
+        }
+        in.close();
     }
 }
