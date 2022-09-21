8547e276939ec86d5000a23976f3062e6b7849b4
Furkan Komaç <furkankomac@gmail.com>
2022-09-21 15:39:03 +0300

    I wrote a code that create the array

--- Lab00_assignment2.java
+++ Lab00_assignment2.java
@@ -1,6 +1,10 @@
 public class Lab00_assignment2 {
 
     public static void main(String[] args) {
-        
+        int[] numbers = new int[101];
+        for(int i = 0;i <= 100; i++){
+            numbers[i] = i;
+        }
+        System.out.println(numbers[101]);
     }
 }
